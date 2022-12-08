ec9202e3b96b41476233cfadb4564b5d5d426abf
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-12-07 17:35:29 -0800

    added 4 routes back

--- app.py
+++ app.py
@@ -4,7 +4,35 @@ from operations import add, sub, mult, div
 app = Flask(__name__)
 
 
-@app.route("/<operation>")
+@app.route("/add")
+def addition():
+    a = int(request.args.get('a', 5))
+    b = int(request.args.get('b', 5))
+    return str(add(a, b))
+
+
+@app.route("/sub")
+def subtraction():
+    a = int(request.args.get('a', 5))
+    b = int(request.args.get('b', 5))
+    return str(sub(a, b))
+
+
+@app.route("/div")
+def division():
+    a = int(request.args.get('a', 5))
+    b = int(request.args.get('b', 5))
+    return str(div(a, b))
+
+
+@app.route("/mult")
+def multiplication():
+    a = int(request.args.get('a', 5))
+    b = int(request.args.get('b', 5))
+    return str(mult(a, b))
+
+
+@app.route("/math/<operation>")
 def calculate(operation):
     oprtns = {'add': add, 'sub': sub, 'mult': mult, 'div': div}
     a = int(request.args.get('a', 5))
