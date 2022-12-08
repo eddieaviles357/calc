40efd8dff976e58be9fb6b4b5441fcf3cff88cc9
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-12-07 16:56:44 -0800

    import Flask, request added, added test functions

--- operations.py
+++ operations.py
@@ -1,21 +1,32 @@
 """Basic math operations."""
 
 def add(a, b):
-    """Add a and b."""
-    
+    """Add a and b.
+    >>> add(3, 5)
+    8
+    """
     return a + b
 
 def sub(a, b):
-    """Substract b from a."""
+    """Substract b from a.
+    >>> sub(5, 3)
+    2
+    """
 
     return a - b
 
 def mult(a, b):
-    """Multiply a and b."""
+    """Multiply a and b.
+    >>> mult(3, 5)
+    15
+    """
 
     return a * b
 
 def div(a, b):
-    """Divide a by b."""
+    """Divide a by b.
+    >>> div(6, 3)
+    2
+    """
 
     return a / b
