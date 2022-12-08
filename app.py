13e8a551b8345d6c109ce5aa5649bd68004ca4b8
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-12-07 17:32:12 -0800

    made one route to handle sub, div, mult, add with no if else statements

--- app.py
+++ app.py
@@ -4,29 +4,13 @@ from operations import add, sub, mult, div
 app = Flask(__name__)
 
 
-@app.route("/add")
-def addition():
+@app.route("/<operation>")
+def calculate(operation):
+    oprtns = {'add': add, 'sub': sub, 'mult': mult, 'div': div}
     a = int(request.args.get('a', 5))
     b = int(request.args.get('b', 5))
-    return str(add(a, b))
-
-
-@app.route("/sub")
-def subtraction():
-    a = int(request.args.get('a', 5))
-    b = int(request.args.get('b', 5))
-    return str(sub(a, b))
-
-
-@app.route("/div")
-def division():
-    a = int(request.args.get('a', 5))
-    b = int(request.args.get('b', 5))
-    return str(div(a, b))
-
-
-@app.route("/mult")
-def multiplication():
-    a = int(request.args.get('a', 5))
-    b = int(request.args.get('b', 5))
-    return str(mult(a, b))
+    return f'<h1>{a} {operation} {b} = {oprtns[operation](a,b)}</h1>'
+    # return str(add(a, b))
+    # return str(sub(a, b))
+    # return str(div(a, b))
+    # return str(mult(a, b))
